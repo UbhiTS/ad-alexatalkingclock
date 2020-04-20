badafa7988e8ba96e77285781f18ddb292483944
Tarunpreet Ubhi <ubhits@outlook.com>
2020-04-19 23:37:44 -0700

    + Whisper, Volume, Pitch, Rate and Bell Controls

--- apps/alexa_talking_clock/alexa_talking_clock.py
+++ apps/alexa_talking_clock/alexa_talking_clock.py
@@ -1,46 +1,63 @@
 import appdaemon.plugins.hass.hassapi as hass
 import datetime
-
+ 
 #
 # Talking Clock AppDeamon App for Home Assistant
 # Developed by @UbhiTS on GitHub
 #
 # Args:
-# alexa_talking_clock:
-#   module: alexa_talking_clock
-#   class: AlexaTalkingClock
-#   alexa: media_player.kitchen_alexa
-#   start_hour: 7
-#   start_minute: 30
-#   end_hour: 21
-#   end_minute: 30
-#   announce_hour: true
-#   announce_half_hour: true
-#   announce_quarter_hour: false
+#alexa_talking_clock:
+#  module: alexa_talking_clock
+#  class: AlexaTalkingClock
+#  alexa: media_player.kitchen_alexa
+#  whisper: false
+#  pitch_offset: 0 # -33 to 50, default 0
+#  volume_offset: 0 # -40 to 4, default 0
+#  rate: 100 # 20 to 250, default 100
+#  announce_bell: true
+#  announce_half_hour: true
+#  announce_quarter_hour: true
+#  start_hour: 7
+#  start_minute: 30
+#  end_hour: 21
+#  end_minute: 30
+#  debug: false
 
 class AlexaTalkingClock(hass.Hass):
 
   def initialize(self):
     
     self.alexa = self.args["alexa"]
-    self.start_hour = self.args["start_hour"]
-    self.start_minute = self.args["start_minute"]
-    self.end_hour = self.args["end_hour"]
-    self.end_minute = self.args["end_minute"]
-    self.announce_hour = self.args["announce_hour"]
-    self.announce_half_hour = self.args["announce_half_hour"]
-    self.announce_quarter_hour = self.args["announce_quarter_hour"]
-
+    self.whisper = bool(self.args["whisper"]) if "whisper" in self.args else False
+    self.pitch_offset = int(self.args["pitch_offset"]) if "pitch_offset" in self.args else 0
+    self.volume_offset = int(self.args["volume_offset"]) if "volume_offset" in self.args else 0
+    self.rate = int(self.args["rate"]) if "rate" in self.args else 100
+    self.announce_bell = bool(self.args["announce_bell"]) if "announce_bell" in self.args else True
+    self.announce_hour = True
+    self.announce_half_hour = bool(self.args["announce_half_hour"]) if "announce_half_hour" in self.args else True
+    self.announce_quarter_hour = bool(self.args["announce_quarter_hour"]) if "announce_quarter_hour" in self.args else False
+    self.start_hour = int(self.args["start_hour"]) if "start_hour" in self.args else 7
+    self.start_minute = int(self.args["start_minute"]) if "start_minute" in self.args else 30
+    self.end_hour = int(self.args["end_hour"]) if "end_hour" in self.args else 21
+    self.end_minute = int(self.args["end_minute"]) if "end_minute" in self.args else 30
+    self.debug = bool(self.args["debug"]) if "debug" in self.args else False
+    
+    if self.pitch_offset < -33: self.pitch_offset = -33
+    if self.pitch_offset > 50: self.pitch_offset = 50
+    if self.volume_offset < -40: self.volume_offset = -40
+    if self.volume_offset > 4: self.volume_offset = 4
+    if self.rate < 20: self.rate = 20
+    if self.rate > 250: self.rate = 250
+    
     self.frequency = self.get_frequency()
     self.next_start = self.get_next_start()
-    
-    if self.frequency.interval is None:
-      raise Exception("ERROR: No announce frequency defined. Please set at least one frequency interval")
     
     self.run_every(self.time_announce, self.next_start, (60 * self.frequency.interval))
     
     self.log("INITIALIZED: Start " + str(self.next_start.strftime("%H:%M:%S")) + ", Frequency " + str(self.frequency.interval) + ", Times " + str(self.frequency.announce_times))
 
+    if self.debug: self.time_announce(None)
+    
 
   def get_frequency(self):
     
@@ -88,11 +105,30 @@ class AlexaTalkingClock(hass.Hass):
 
   def time_announce(self, kwargs):
     now = datetime.datetime.now()
-    msg = self.time_to_text(now.hour, now.minute)
+    time_speech = self.time_to_text(now.hour, now.minute)
+    
+    if time_speech is not None:
+      msg = self.set_speech_parameters(time_speech)
+      self.log("HOUR_ANNOUNCE_MESSAGE " + time_speech)
+      self.call_service("notify/alexa_media", data = {"type": "announce" if self.announce_bell else "tts", "method": "all"}, target = self.alexa, message = msg)
 
-    if msg is not None:
-      self.log("HOUR_ANNOUNCE_MESSAGE " + msg)
-      self.call_service("notify/alexa_media", data = {"type":"announce", "method":"all"}, target = self.alexa, message = msg)
+
+  def set_speech_parameters(self, time_speech):
+    prefix = "<speak>"
+    postfix = "</speak>"
+    
+    if self.whisper:
+      prefix = prefix + "<amazon:effect name='whispered'>"
+      postfix = "</amazon:effect>" + postfix
+
+    str_rate = str(self.rate)
+    str_pitch = "+" + str(self.pitch_offset) if self.pitch_offset >= 0 else str(self.pitch_offset)
+    str_volume = "+" + str(self.volume_offset) if self.volume_offset >= 0 else str(self.volume_offset)
+  
+    prefix = prefix + "<prosody rate='" + str_rate + "%' pitch='" + str_pitch + "%' volume='" + str_volume + "dB'>"
+    postfix = "</prosody>" + postfix
+      
+    return prefix + time_speech + postfix
 
 
   def time_to_text(self, hour, minute):
@@ -101,12 +137,13 @@ class AlexaTalkingClock(hass.Hass):
     postfix = ""
     time_speech = ""
     
-    if hour < self.start_hour or hour > self.end_hour:
-      return
-    if hour == self.start_hour and minute < self.start_minute:
-      return
-    if hour == self.end_hour and minute > self.end_minute:
-      return
+    if not self.debug:
+      if hour < self.start_hour or hour > self.end_hour:
+        return
+      if hour == self.start_hour and minute < self.start_minute:
+        return
+      if hour == self.end_hour and minute > self.end_minute:
+        return
 
     ampm_str = "AM" if hour <= 11 else "PM"
     
@@ -122,9 +159,9 @@ class AlexaTalkingClock(hass.Hass):
     hour = hour - 12 if hour > 12 else hour
   
     if minute == 0:
-      time_speech = "It's " + str(hour) + " " + ampm_str + "."
+      time_speech = f"It's {hour} {ampm_str}."
     else:
-      time_speech = "It's " + str(hour) + ":" + str(minute) + " " + ampm_str + "."
+      time_speech = f"It's {hour}:{minute:02d} {ampm_str}."
     
     return prefix + " " + time_speech + " " + postfix
 
